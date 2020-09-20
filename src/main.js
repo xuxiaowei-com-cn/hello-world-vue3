af42a16bba75d8d63a9942633015b3d9734be2ae
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-20 10:40:03 +0800

    format

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
 import router from './router'
 import store from './store'
 import axios from './axios'
+
 import './scss/style.scss'
 
 const app = createApp(App)
-app.use(store).use(router).mount('#app')
+
+app.use(store)
+app.use(router)
+
+app.mount('#app')
 
 app.config.globalProperties.$axios = axios
