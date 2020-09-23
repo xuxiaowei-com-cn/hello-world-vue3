95160b6b8c76f8aafde6bc94a236c15d69b835ad
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-23 14:56:11 +0800

    Clean

--- src/router/demo/index.js
+++ src/router/demo/index.js
@@ -6,14 +6,6 @@ const demoAxios =
         path: '/demo/axios',
         name: 'Axios',
         component: Axios,
-        meta: {
-            isLogin: false,
-            useFrame: { //  是否使用框架
-                aside: false, // 左侧栏是否显示
-                header: false, // 头部栏是否显示
-                footer: true, // 脚部栏是否显示
-            },
-        }
     }
 
 const demoVant =
@@ -21,14 +13,6 @@ const demoVant =
         path: '/demo/vant',
         name: 'Vant',
         component: Vant,
-        meta: {
-            isLogin: false,
-            useFrame: { //  是否使用框架
-                aside: false, // 左侧栏是否显示
-                header: false, // 头部栏是否显示
-                footer: true, // 脚部栏是否显示
-            },
-        }
     }
 
 export {
